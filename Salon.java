93484cf3dd548abec0a602a3c8b0981d79ce21dc
José Manuel <53132421+jmosted@users.noreply.github.com>
2019-09-09 10:13:44 -0500

    Update Salon.java
    New Code!

--- Salon.java
+++ Salon.java
@@ -10,5 +10,34 @@ package Roman;
  * @author PC
  */
 public class Salon {
+    private int id;
+    private int aforo;
+
+    public Salon(int id, int aforo) {
+        this.id = id;
+        this.aforo = aforo;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getAforo() {
+        return aforo;
+    }
+
+    public void setAforo(int aforo) {
+        this.aforo = aforo;
+    }
+
+    @Override
+    public String toString() {
+        return "Salon{" + "id=" + id + ", aforo=" + aforo + '}';
+    }
+    
     
 }
